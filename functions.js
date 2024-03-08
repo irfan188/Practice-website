c8428f3ecdf771c4556fc4d54ef95a77c6750617
codezilla7 <mm3427129@gmail.com>
2024-03-09 00:04:09 +0500

    array methods

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 // function declaration
 
-const func = document.querySelector(".parent");
+// const func = document.querySelector(".parent");
 
-alertBtn();
-function alertBtn(value) {
-  func.style.background = "blue";
-}
+// alertBtn();
+// function alertBtn(value) {
+//   func.style.background = "blue";
+// }
 
 // function expression
 
@@ -30,29 +30,29 @@ function alertBtn(value) {
 //   return result;
 // };
 
-const myfunction = function (salary = 10000, tax = 938) {
-  return salary - tax;
-};
+// const myfunction = function (salary = 10000, tax = 938) {
+//   return salary - tax;
+// };
 
-console.log(myfunction(90000, 837));
+// console.log(myfunction(90000, 837));
 
-myfunction(30000, 945);
+// myfunction(30000, 945);
 
-myfunction(60000, 893);
+// myfunction(60000, 893);
 
-// regular function
+// // regular function
 
-function buttonToggle() {
-  console.log("value");
-}
+// function buttonToggle() {
+//   console.log("value");
+// }
 
-buttonToggle();
+// buttonToggle();
 
-// arrow function
+// // arrow function
 
-const menuToggle = (score) => console.log(score);
+// const menuToggle = (score) => console.log(score);
 
-menuToggle(99);
+// menuToggle(99);
 
 // practice arrow functions
 
@@ -64,16 +64,16 @@ menuToggle(99);
 //   return total;
 // };
 
-const bill = (products, tax) => {
-  let total = 0;
-  for (let i = 0; i < products.length; i++){
-    total += products[i] + products[i] * tax;
-  }
-  return total;
-};
+// const bill = (products, tax) => {
+//   let total = 0;
+//   for (let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// };
 
 
-console.log(bill([10, 15, 30], 0.2))
+// console.log(bill([10, 15, 30], 0.2))
 
 // console.log(bill([10, 15, 30], 0.2));
 
@@ -95,10 +95,46 @@ console.log(bill([10, 15, 30], 0.2))
 
 // callbacks & forEach 
 
-const myfunc = (callbackFunc) => {
-  // do something
-  let value = 50;
-  callbackFunc(value)
-}
+// const myfunc = (callbackFunc) => {
+//   // do something
+//   let value = 50;
+//   callbackFunc(value)
+// }
 
-myfunc(value => console.log(value))
+// myfunc(value => console.log(value))
+
+// const test = (callbacktest) => {
+//   let value2 = "jan";
+//   callbacktest(value2)
+// }
+// test(value2 => console.log(value2))
+
+
+// const loop = (test) => {
+//   let teacher = ['khalil', 'jan ali', 'rezaie'];
+//   for(let i = 0; i < teacher.length; i++) {
+//     let result = teacher[i]
+//     test(result)
+//   }
+// }
+// loop (test  => console.log(test))
+
+
+
+// array Methods with callBacks
+
+// const customers = ["khalil", "qadir", "jawad", "zia", "sajjad"]
+
+// customers.forEach(person => console.log(person))
+
+// customers.map(person => console.log(person))
+
+// const filterEle = "jawad";
+
+// const filteredArr = customers.filter(person => person !== filterEle)
+
+// console.log(filteredArr)
+
+// const filterFind = customers.find(person => person === "zia")
+
+// console.log(filterFind)
