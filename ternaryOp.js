5b81396ca77fe7b9840018f76db31a3305ff75d3
codezilla7 <mm3427129@gmail.com>
2024-03-24 22:56:16 +0500

    objects,this and funtion

--- ternaryOp.js
+++ ternaryOp.js
@@ -4,17 +4,17 @@ const themeToggle = function (value) {
   object.style.background = value === "on" ? "red" : "blue";
 };
 
-let myfunction = function (value) {
-  for (let i = 1; i <= value; i++) {
-    let result = "";
-    for (let j = 1; j <= i; j++) {
-      result += `${j} `;
-    }
-    console.log(result);
-  }
-};
+// let myfunction = function (value) {
+//   for (let i = 1; i <= value; i++) {
+//     let result = "";
+//     for (let j = 1; j <= i; j++) {
+//       result += `${j} `;
+//     }
+//     console.log(result);
+//   }
+// };
 
-myfunction(8);
+// myfunction(8);
 
 const object2 = [
   {
@@ -41,4 +41,22 @@ let date = new Date()
 
 let math = Math
 
-console.log(math.ceil(3.4))
+// console.log(math.ceil(3.4))
+
+
+const users = {
+  name: "sajjad",
+  age: 20,
+  male: true,
+  female: false,
+  phoneNumbers: ["423424234","34324234"],
+  logIn(){
+    console.log(this)
+    this.phoneNumbers.forEach(num => console.log(num))
+  }
+}
+
+users.logIn();
+
+
+console.log(this)
